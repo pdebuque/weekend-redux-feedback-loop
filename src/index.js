36bf2b49fb299b3f9f79eef7dd7e2e0005fa8213
Paolo Debuque <pdebuque@gmail.com>
2022-12-01 19:49:56 -0600

    set up index.js boilerplate

--- src/index.js
+++ src/index.js
@@ -4,5 +4,29 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux stuff import
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+//router stuff import
+import { BrowserRouter } from 'react-router-dom'
+
+// redux stuff boilerplate
+
+const feelings = (state = '', action) => {
+    return state
+}
+
+const store = createStore(combineReducers({
+    feelings,
+}), applyMiddleware(logger))
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
